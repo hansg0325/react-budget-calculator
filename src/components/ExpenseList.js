50484180974995c2a6802e18bcb2ba1248737809
hansg0325 <hansg0325@gmail.com>
2023-09-26 18:36:42 +0900

    initial

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,31 @@
+import "./ExpenseList.css";
+import { MdDelete } from "react-icons/md";
+import ExpenseItem from "./ExpenseItem";
+import React from "react";
+
+const ExpenseList = ({ expenses, handleDelete, handleEdit, clearItems }) => {
+  return (
+    <>
+      <ul className="list">
+        {expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              expense={expense}
+              handleDelete={handleDelete}
+              hendleEdit={handleEdit}
+            />
+          );
+        })}
+      </ul>
+      {expenses.length > 0 && (
+        <button className="btn" onClick={clearItems}>
+          목록 지우기
+          <MdDelete className="btn-icon" />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ExpenseList;
